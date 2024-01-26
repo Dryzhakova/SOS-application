2e020622e0fb74e9466288952b7ea84a411f97c0
Hererra <93358288+Hererra@users.noreply.github.com>
2024-01-26 03:14:25 +0100

    Added SettingsActivity

--- app/src/main/java/com/example/project_android/MainActivity.kt
+++ app/src/main/java/com/example/project_android/MainActivity.kt
@@ -8,17 +8,25 @@ import com.example.project_android.Location.LocationHelper
 import com.example.project_android.Message.SMS
 import android.Manifest
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.hardware.SensorManager
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 import com.example.project_android.Timer.Timer
 import kotlin.math.abs
 import kotlin.math.sqrt
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     // MiniTasks:
     // 1. Пока что таймер просто запускается, но его никак не выключить. Кроме как, кнопкой TurnOff
@@ -42,12 +50,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         // initialization buttons and TextView
         turnOn = findViewById(R.id.turnOnButton)
         turnOff = findViewById(R.id.turnOffButton)
         status = findViewById(R.id.statusTextView)
         gpsTextView = findViewById(R.id.gpsTextView)
         accTextView = findViewById(R.id.accTextView)
+
+
 
 
 
@@ -90,6 +103,29 @@ class MainActivity : ComponentActivity() {
 
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    // Обрабатываем нажатие на пункт меню
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Открываем SettingsActivity при нажатии на кнопку настроек
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            android.R.id.home -> {
+                // Обработка нажатия на кнопку "назад" в ActionBar
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     // Checking Permissions
     private fun checkPermissions(): Boolean{
         val permissions = arrayOf(
@@ -121,10 +157,18 @@ class MainActivity : ComponentActivity() {
         if (lastAcceleration != null) {
             val accelerationChange = calculateAccelerationChange(acceleration, lastAcceleration!!)
 
-            // Это бы перенести в SharedPreferences для настройки чувствительности из меню настроек(activity меню надо будет создать еще)
-            val threshold = 5.0
-            if (accelerationChange > threshold) {
+            val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+            // Retrieve the accelerometer sensitivity directly as an integer
+            val accelerometerSensitivity: Int = try {
+                preferences.getInt("accelerometer_sensitivity", 5)
+            } catch (e: ClassCastException) {
+                // Handle exception, if necessary
+                5
+            }
+            if (accelerationChange > accelerometerSensitivity) {
                 // Изменились данные акселерометра
+                Log.d("Ac", "Сработало acceleration!")
                 lastAcceleration = acceleration
                 accTextView.text = "Acceleration: $lastAcceleration"
                 return true
@@ -133,7 +177,9 @@ class MainActivity : ComponentActivity() {
 
         // Сохранение новых данных акселерометра(обновляются каждые 200 мс)
         lastAcceleration = acceleration
-        accTextView.text = "Acceleration: $lastAcceleration"
+        val accelerationString = "x: ${acceleration[0]}, y: ${acceleration[1]}, z: ${acceleration[2]}"
+        accTextView.text = "Acceleration: $accelerationString"
+
         return false
     }
 
@@ -144,9 +190,12 @@ class MainActivity : ComponentActivity() {
         for (i in 0 until 3) {
             deltaAcceleration[i] = acceleration1[i] - acceleration2[i]
         }
-        return sqrt((deltaAcceleration[0] * deltaAcceleration[0]
+
+        val value = sqrt((deltaAcceleration[0] * deltaAcceleration[0]
                 + deltaAcceleration[1] * deltaAcceleration[1]
                 + deltaAcceleration[2] * deltaAcceleration[2]).toDouble())
+        return value
+
     }
 
 
@@ -156,19 +205,22 @@ class MainActivity : ComponentActivity() {
             val deltaLatitude = abs(latitude - lastLocation!!.first)
             val deltaLongitude = abs(longitude - lastLocation!!.second)
 
-            // Это бы перенести в SharedPreferences для настройки чувствительности из меню настроек(activity меню надо будет создать еще)
-            val threshold = 0.0001
+            val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+            val gpsSensitivity: Double =
+                (preferences.getInt("gps_sensitivity", 10) * 0.00001)
 
-            if (deltaLatitude > threshold || deltaLongitude > threshold) {
+            if (deltaLatitude > gpsSensitivity || deltaLongitude > gpsSensitivity) {
                 // Изменились данные GPS
                 lastLocation = Pair(latitude, longitude)
 
                 gpsTextView.text = "GPS: $lastLocation"
 
                 // For Testing
-               // showFallDetectedDialog()
+                Log.d("GPS", "Сработало GPS!")
+                showFallDetectedDialog()
                 return true
             }
+            Log.d("GPS", "No location")
         }
 
         // Сохраняем текущие значения координат
