b0486312ec168c8dda7ecb003537ec68b0b5672f
Azami <99400189+ArseniMokhau@users.noreply.github.com>
2024-01-31 19:31:19 +0100

    Initial Changes

--- app/src/main/java/com/example/project_android/BackgroundService.kt
+++ app/src/main/java/com/example/project_android/BackgroundService.kt
@@ -1,37 +1,21 @@
 package com.example.project_android
 
 import android.Manifest
-import android.app.AlertDialog
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
 import android.media.MediaPlayer
-import android.os.Build
-import android.os.Bundle
-import android.os.Handler
-import android.os.IBinder
-import android.os.Looper
+import android.os.*
+import android.preference.PreferenceManager
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.util.Log
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.preference.PreferenceManager
 import com.example.project_android.Accelerometer.AccelerometerListener
 import com.example.project_android.Contacts.ContactsDetails
 import com.example.project_android.Location.LocationHelper
-import com.example.project_android.MainActivity
 import com.example.project_android.Message.SMS
 import com.example.project_android.Timer.Timer
 import kotlin.math.abs
@@ -42,21 +26,20 @@ class BackgroundService : Service() {
     private lateinit var locationHelper: LocationHelper
     private lateinit var smsSender: SMS
     private lateinit var timer: Timer
-    private var coordinateLatitude : String = ""
-    private var coordinateLongtitude : String = ""
-
+    private var coordinateLatitude: String = ""
+    private var coordinateLongitude: String = ""
 
     private var timerInstance: Timer? = null
-    private var mediaPlayer : MediaPlayer? = null
-    private var isMediaPlayerStarted : Boolean = false
-    private var isSecondMediaPlayerStarted : Boolean = false
+    private var mediaPlayer: MediaPlayer? = null
+    private var isMediaPlayerStarted: Boolean = false
     private lateinit var speechRecognizer: SpeechRecognizer
 
     private var SpeechTimeHandler = Handler(Looper.getMainLooper())
-    private var SpeechTimerRunnable : Runnable? = null
+    private var SpeechTimerRunnable: Runnable? = null
     private var shakeTimerHandler = Handler(Looper.getMainLooper())
     private var shakeTimerRunnable: Runnable? = null
     private var isTimerStarted: Boolean = false
+    private var isAccelerationDetected: Boolean = false
 
     private var lastAcceleration: FloatArray? = null
     private var lastLocation: Pair<Double, Double>? = null
@@ -64,34 +47,21 @@ class BackgroundService : Service() {
 
     companion object {
         private const val NOTIFICATION_ID = 1
+        private const val TURN_OFF_ACTION = "TURN_OFF_ACTION"
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // Вызывается при старте службы
-
-
-
-        if (intent?.action == "TURN_OFF_ACTION") {
+        if (intent?.action == TURN_OFF_ACTION) {
             turnOFF()
         }
 
-
         Log.d("MainActivity", "Кракен готовится")
 
-
-        // start speech
-
-
-
-
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-
         val message = sharedPreferences.getString(SettingsActivity.PREFS_KEY_SELECTED_MESSAGE, "")
         ContactsDetails.message = message.toString()
         val number = sharedPreferences.getString(SettingsActivity.PREFS_KEY_SELECTED_CONTACT_NUMBER, "")
         ContactsDetails.number = number.toString()
-
-
 
         accelerometerListener = AccelerometerListener(this) { acceleration ->
             onAccelerationChanged(acceleration)
@@ -101,24 +71,18 @@ class BackgroundService : Service() {
             if (checkLocation(latitude, longitude)) {
                 locationHelper.stopLocationUpdates()
                 isMediaPlayerStarted = false
-                isSecondMediaPlayerStarted = false
             }
         }
 
         smsSender = SMS(this)
 
         timer = Timer {
-//            showFallDetectedDialog()
             smsSender.sendSMS("", "")
         }
 
         accelerometerListener.register()
 
-
-
-
-        val notification =
-            createNotification()  // Создайте уведомление, которое будет отображаться в статус-баре
+        val notification = createNotification()
         startForeground(NOTIFICATION_ID, notification)
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
@@ -153,14 +117,25 @@ class BackgroundService : Service() {
                     Log.d("Speech",  spokenText)
 
                     // Check if the spoken text matches predefined phrases
-                    if (spokenText =="stop") {
-                        Log.d("Speech","Выполнено!")
-                        turnOFF()
-
-                    } else if (spokenText == "help") {
-                        smsSender.sendSMS(ContactsDetails.message, ContactsDetails.number)
-                        turnOFF()
-                        Log.d("SpeechHelp",  "Help is on the way!")
+                    when (spokenText) {
+                        "stop" -> {
+                            Log.d("Speech", "Выполнено!")
+                            playTimerStoppedSound()
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                turnOFF()
+                            }, 1000)
+                        }
+                        "help" -> {
+                            playEnteringSOSModeSound()
+                            smsSender.sendSMS(ContactsDetails.message, ContactsDetails.number)
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                turnOFF()
+                            }, 3000)
+                            Log.d("SpeechHelp", "Help is on the way!")
+                        }
+                        else -> {
+                            // Handle other cases if needed
+                        }
                     }
                 }
             }
@@ -174,26 +149,19 @@ class BackgroundService : Service() {
             }
         })
 
-
-            // Обработка случая, когда разрешения не получены
-
-
-//        loadSelectedContact()
-
-        return START_STICKY // Служба будет перезапущена в случае ее аварийной остановки
+        return START_STICKY
     }
 
-
-    fun turnOFF(){
+    private fun turnOFF() {
         synchronized(this) {
             locationHelper.stopLocationUpdates()
             timerInstance?.cancelTimer()
+            stopSelf()  // Add this line to stop the service
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // Вызывается при остановке службы
         accelerometerListener.unregister()
         locationHelper.stopLocationUpdates()
         timer.cancelTimer()
@@ -205,22 +173,38 @@ class BackgroundService : Service() {
         return null
     }
 
+    private fun playImpactSound() {
+        // Release the previous MediaPlayer instance
+        mediaPlayer?.release()
 
-    private fun onAccelerationChanged(acceleration: FloatArray) {
-        if (checkAcceleration(acceleration))
-        {
-
-            startShakeTimer()
-
-
-                // Create a new MediaPlayer instance and start playing the specified MP3
-
-
-
-
-        }
+        // Create a new MediaPlayer instance
+        mediaPlayer = MediaPlayer.create(this, R.raw.impact_detected)
+        mediaPlayer?.start()
     }
 
+    private fun playTimerStoppedSound() {
+        // Release the previous MediaPlayer instance
+        mediaPlayer?.release()
+
+        // Create a new MediaPlayer instance
+        mediaPlayer = MediaPlayer.create(this, R.raw.timer_stopped)
+        mediaPlayer?.start()
+    }
+
+    private fun playEnteringSOSModeSound() {
+        // Release the previous MediaPlayer instance
+        mediaPlayer?.release()
+
+        // Create a new MediaPlayer instance
+        mediaPlayer = MediaPlayer.create(this, R.raw.entering_sos_mode)
+        mediaPlayer?.start()
+    }
+
+    private fun onAccelerationChanged(acceleration: FloatArray) {
+        if (checkAcceleration(acceleration)) {
+            startShakeTimer()
+        }
+    }
 
     private fun startVoiceRecognition() {
         // Start the voice recognition process
@@ -242,17 +226,11 @@ class BackgroundService : Service() {
 
         speechRecognizer.startListening(intent)
     }
-//
 
     private fun startShakeTimer() {
         shakeTimerRunnable?.let { shakeTimerHandler.removeCallbacks(it) }
 
         shakeTimerRunnable = Runnable {
-//            showFallDetectedDialog()
-            isTimerStarted = false
-
-
-
             locationHelper.requestLocationUpdates()
             shakeTimerRunnable = null
         }
@@ -260,17 +238,12 @@ class BackgroundService : Service() {
         shakeTimerHandler.postDelayed(shakeTimerRunnable!!, 3000)
     }
 
-
     private fun startSpeechTimer() {
+
         SpeechTimerRunnable?.let { SpeechTimeHandler.removeCallbacks(it) }
 
         SpeechTimerRunnable = Runnable {
-//            showFallDetectedDialog()
-
-
             startVoiceRecognition()
-
-
             SpeechTimerRunnable = null
         }
 
@@ -278,36 +251,20 @@ class BackgroundService : Service() {
     }
 
     private fun checkAcceleration(acceleration: FloatArray): Boolean {
-        if (lastAcceleration != null) {
+        if (lastAcceleration != null && !isAccelerationDetected) {
             val accelerationChange = calculateAccelerationChange(acceleration, lastAcceleration!!)
 
             val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-
-            // Retrieve the accelerometer sensitivity directly as an integer
-            val accelerometerSensitivity: Int = try {
-                preferences.getInt("accelerometer_sensitivity", 5)
-
-
-            } catch (e: ClassCastException) {
-                // Handle exception, if necessary
-                5
-            }
+            val accelerometerSensitivity: Int = preferences.getInt("accelerometer_sensitivity", 5)
 
             if (accelerationChange > accelerometerSensitivity) {
-                // Изменились данные акселерометра
-                Log.d("Ac", "Сработало acceleration!")
                 lastAcceleration = acceleration
-                Log.d("Ac", "x: ${acceleration[0]}, y: ${acceleration[1]}, z: ${acceleration[2]}")
+                isAccelerationDetected = true
                 return true
             }
         }
 
-        // Сохранение новых данных акселерометра(обновляются каждые 200 мс)
         lastAcceleration = acceleration
-        val accelerationString =
-            "x: ${acceleration[0]}, y: ${acceleration[1]}, z: ${acceleration[2]}"
-
-
         return false
     }
 
@@ -329,25 +286,7 @@ class BackgroundService : Service() {
 
     }
 
-//    private fun SecondMediaPlayer(){
-//        if(!isSecondMediaPlayerStarted) {
-//
-//            isSecondMediaPlayerStarted = true
-//        }
-//    }
-
-    private fun MediaPlayer(){
-        if(!isMediaPlayerStarted) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.impact_detected)
-            mediaPlayer?.start()
-            isMediaPlayerStarted = true
-        }
-    }
-
     private fun checkLocation(latitude: Double, longitude: Double): Boolean {
-
-
-
         if (lastLocation != null) {
             val deltaLatitude = abs(latitude - lastLocation!!.first)
             val deltaLongitude = abs(longitude - lastLocation!!.second)
@@ -356,12 +295,13 @@ class BackgroundService : Service() {
             val gpsSensitivity: Double =
                 (preferences.getInt("gps_sensitivity", 10) * 0.00001)
 
-            val count : Int = (preferences.getInt("set_timer", 10) * 1000)
+            val count : Int = ((preferences.getInt("set_timer", 10) + 5)* 1000)
             Log.d("Message", count.toString())
 
             if (deltaLatitude > gpsSensitivity || deltaLongitude > gpsSensitivity) {
                 // Изменились данные GPS
                 lastLocation = Pair(latitude, longitude)
+                isAccelerationDetected = false
 
 
                 // For Testing
@@ -370,26 +310,21 @@ class BackgroundService : Service() {
             }
             Log.d("GPS", "No location")
             coordinateLatitude = latitude.toString()
-            coordinateLongtitude = longitude.toString()
+            coordinateLongitude = longitude.toString()
             Log.d("coordinateLatitude", coordinateLatitude)
-            Log.d("coordinateLongtitude", coordinateLongtitude)
+            Log.d("coordinateLongtitude", coordinateLongitude)
             lastLocation = Pair(latitude, longitude)
 
-            ContactsDetails.message = ContactsDetails.message + "\nCoordinates: $coordinateLatitude,$coordinateLatitude"
+            ContactsDetails.message = ContactsDetails.message + "\nCoordinates: $coordinateLatitude,$coordinateLongitude"
 
-            MediaPlayer()
+            playImpactSound()
             startSpeechTimer()
 
 
             if (!isTimerStarted) {
                 val timer = Timer {
-                    mediaPlayer = MediaPlayer.create(this, R.raw.entering_sms_mode)
-                    mediaPlayer?.start()
+                    playEnteringSOSModeSound()
                     smsSender.sendSMS(ContactsDetails.message, ContactsDetails.number)
-
-
-
-
                 }
                 isTimerStarted = true
                 timer.startTimer(count.toLong())
@@ -401,63 +336,36 @@ class BackgroundService : Service() {
         }
 
         // Сохраняем текущие значения координат
-            lastLocation = Pair(latitude, longitude)
+        lastLocation = Pair(latitude, longitude)
 
-            return false
+        return false
+    }
+
+    private fun createNotification(): Notification {
+        val channelId = "1"
+        val channelName = "Name"
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
         }
 
+        val notificationIntent = Intent(this, BackgroundService::class.java)
+        notificationIntent.action = TURN_OFF_ACTION
+        val pendingIntent =
+            PendingIntent.getService(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
 
-        private fun createNotification(): Notification {
-            val channelId = "1"
-            val channelName = "Name"
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Foreground Service")
+            .setContentText("Service is running in foreground")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentIntent(pendingIntent)
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val channel = NotificationChannel(
-                    channelId,
-                    channelName,
-                    NotificationManager.IMPORTANCE_DEFAULT
-                )
-                notificationManager.createNotificationChannel(channel)
-            }
-
-            val notificationIntent = Intent(this, BackgroundService::class.java)
-            notificationIntent.action = "TURN_OFF_ACTION" // Уникальный action для вашего PendingIntent
-            val pendingIntent = PendingIntent.getService(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
-
-            val builder = NotificationCompat.Builder(this, channelId)
-                .setContentTitle("Foreground Service")
-                .setContentText("Service is running in foreground")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentIntent(pendingIntent)// Укажите свою иконку уведомления
-
-            return builder.build()
-        }
-
-
-
-
-
-
-
+        return builder.build()
+    }
 }
-
-
-
-//    private fun showFallDetectedDialog() {
-//        val builder = AlertDialog.Builder(this)
-//        builder.setTitle("Wykryt Wypadek")
-//        builder.setMessage("Było wykryto wypadek. Wezwać pomoc?")
-//        builder.setPositiveButton("Да") { _, _ ->
-//            // Здесь можно добавить код для вызова помощи
-//            // Например, отправка SMS с предупреждением о падении
-//        }
-//        builder.setNegativeButton("Нет") { _, _ ->
-//            // Здесь можно добавить код для отмены вызова помощи
-//            // Например, отправка SMS с отменой вызова
-//        }
-//        builder.show()
-//    }
-
-
--- app/src/main/java/com/example/project_android/MainActivity.kt
+++ app/src/main/java/com/example/project_android/MainActivity.kt
@@ -1,21 +1,10 @@
 package com.example.project_android
 
-
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import com.example.project_android.Accelerometer.AccelerometerListener
-import com.example.project_android.Location.LocationHelper
-import com.example.project_android.Message.SMS
-import android.Manifest
-import android.app.AlertDialog
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
-import android.hardware.SensorManager
 import android.os.Build
-import android.os.Handler
-import android.os.Looper
+import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -23,26 +12,17 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import com.example.project_android.Contacts.ContactsDetails
-import com.example.project_android.Timer.Timer
-import kotlin.math.abs
-import kotlin.math.sqrt
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
 
-    // MiniTasks:
-    // 1. Пока что таймер просто запускается, но его никак не выключить. Кроме как, кнопкой TurnOff
-
-    private var shakeTimerHandler = Handler(Looper.getMainLooper())
-    private var shakeTimerRunnable: Runnable? = null
-    private var isTimerStarted : Boolean = false
-
-    val permissionsBelowAndroid14 = arrayOf(
+    private val permissionsBelowAndroid14 = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.SEND_SMS,
@@ -51,10 +31,9 @@ class MainActivity : AppCompatActivity() {
         Manifest.permission.RECORD_AUDIO
     )
 
-    // Для Android 14 и выше
-
+    // For Android 14 and above
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
-    val permissionsAndroid14AndAbove = arrayOf(
+    private val permissionsAndroid14AndAbove = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.SEND_SMS,
@@ -68,24 +47,14 @@ class MainActivity : AppCompatActivity() {
 
     private val requestCodePermissions = 123
 
-    public lateinit var sharedPreferences: SharedPreferences
+    private var isTimerStarted: Boolean = false
 
-    private lateinit var accelerometerListener: AccelerometerListener
-    private lateinit var locationHelper: LocationHelper
-    private lateinit var smsSender: SMS
-    private lateinit var timer: Timer
-    // Elements of layout //
+    private lateinit var sharedPreferences: SharedPreferences
 
-    private lateinit var toggleButton : Button
-    private lateinit var turnOn : Button
-    private lateinit var turnOff : Button
-    private lateinit var status : TextView
-
+    private lateinit var toggleButton: Button
+    private lateinit var status: TextView
     private lateinit var gpsTextView: TextView
     private lateinit var accTextView: TextView
-
-    private var lastAcceleration: FloatArray? = null
-    private var lastLocation: Pair<Double, Double>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -96,82 +65,57 @@ class MainActivity : AppCompatActivity() {
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
-        // initialization buttons and TextView
         toggleButton = findViewById(R.id.toggleButton)
         status = findViewById(R.id.statusTextView)
         gpsTextView = findViewById(R.id.gpsTextView)
         accTextView = findViewById(R.id.accTextView)
 
-
         ContactsDetails.message = sharedPreferences.getString("selected_message", "").toString()
         ContactsDetails.number = sharedPreferences.getString("selected_contact_number", "").toString()
-
 
         Log.d("Message", ContactsDetails.message)
         Log.d("Message", ContactsDetails.number)
 
-
-
-
-
-
         toggleButton.setOnClickListener {
-            if (isTimerStarted) {
-                // Code to perform action when turning OFF
-                status.text = "Status: System turned OFF"
-                val serviceIntent = Intent(this, BackgroundService::class.java)
-                stopService(serviceIntent)
-            } else {
-                // Code to perform action when turning ON
-                if (checkPermissions()) {
-                    if(ContactsDetails.number != "" && ContactsDetails.message != "") {
-                        status.text = "Status: System turned ON"
-                        val serviceIntent = Intent(this, BackgroundService::class.java)
-                        startService(serviceIntent)
-                        Log.d("MainActivity", "Kraken is on the hunt")
-                    }else{
-                        showErrorNameMessage()
+            toggleSystem()
+        }
+    }
 
-                    }
+    private fun toggleSystem() {
+        if (isTimerStarted) {
+            status.text = "Status: System turned OFF"
+            stopBackgroundService()
+        } else {
+            if (checkPermissions()) {
+                if (ContactsDetails.number != "" && ContactsDetails.message != "") {
+                    status.text = "Status: System turned ON"
+                    startBackgroundService()
+                    Log.d("MainActivity", "Kraken is on the hunt")
                 } else {
-                    // Handle case when permissions are not granted
+                    showErrorNameMessage()
                 }
             }
-            // Toggle the state
-            isTimerStarted = !isTimerStarted
         }
-
-
-
+        isTimerStarted = !isTimerStarted
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
+    private fun startBackgroundService() {
+        val serviceIntent = Intent(this, BackgroundService::class.java)
+        startService(serviceIntent)
     }
 
-    // Обрабатываем нажатие на пункт меню
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> {
-                // Открываем SettingsActivity при нажатии на кнопку настроек
-                startActivity(Intent(this, SettingsActivity::class.java))
-                true
-            }
-            android.R.id.home -> {
-                // Обработка нажатия на кнопку "назад" в ActionBar
-                onBackPressed()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun stopBackgroundService() {
+        val serviceIntent = Intent(this, BackgroundService::class.java)
+        stopService(serviceIntent)
     }
 
-    // Checking Permissions
-
-
-
+    private fun showErrorNameMessage() {
+        Toast.makeText(
+            this,
+            "Не вписаны номер телефона и сообщение",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
 
     private fun checkPermissions(): Boolean {
         val missingPermissions = getMissingPermissions()
@@ -184,7 +128,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun getMissingPermissions(): Array<String> {
         val missingPermissions = mutableListOf<String>()
 
@@ -195,9 +138,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         for (permission in currentPermissions) {
-            if (ContextCompat.checkSelfPermission(this, permission)
-                != PackageManager.PERMISSION_GRANTED
-            ) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 missingPermissions.add(permission)
             }
         }
@@ -209,88 +150,32 @@ class MainActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(this, permissions, requestCodePermissions)
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == requestCodePermissions) {
-            val deniedPermissions = mutableListOf<String>()
-
-            for ((index, result) in grantResults.withIndex()) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    deniedPermissions.add(permissions[index])
-                }
-            }
-
-            if (deniedPermissions.isNotEmpty()) {
-                showPermissionDeniedToast()
-            } else {
-                // Разрешения предоставлены
-                // Ваш код здесь
-            }
-        }
-    }
-
     private fun showPermissionDeniedToast() {
         Toast.makeText(
             this,
-            "Необходимые разрешения не были предоставлены",
-            Toast.LENGTH_SHORT
-        ).show()
-    }
-    private fun showErrorNameMessage() {
-        Toast.makeText(
-            this,
-            "Не вписаны номер телефона и сообщение",
+            "Please allow all the necessary permissions",
             Toast.LENGTH_SHORT
         ).show()
     }
 
-
-
-
-
-    // При изменении данных с акселерометра вызывает проверку GPS
-
-
-
-
-
-
-
-
-
-
-
-    // Method Euclidian for calculating acceleration change
-
-
-
-
-
-    override fun onResume() {
-        super.onResume()
-//        if (::accelerometerListener.isInitialized) {
-//            accelerometerListener.register()
-//        }
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 
-    override fun onPause() {
-        super.onPause()
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
-    // For testing
-
-
-
-
-
-
-
+    // Other lifecycle methods...
 }
-
